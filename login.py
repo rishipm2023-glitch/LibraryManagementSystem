3c8adcf1afa044e5ef6a33fcc72252698081160d
RISHI <rishianumuthu2006@gmail.com>
2026-01-21 22:33:23 +0530

    Ver3.0

--- login.py
+++ login.py
@@ -1,20 +1,43 @@
-# Version 2 - Multiple Users Login
+# Version 3 - File Based Login System
 
 import getpass
 
-print("===== Library Management System =====")
+MAX_ATTEMPTS = 3
 
-users = {
-    "admin": "library123",
-    "student": "stud123",
-    "staff": "staff123"
-}
+def load_users():
+    users = {}
+    try:
+        with open("users.txt", "r") as file:
+            for line in file:
+                username, password = line.strip().split(",")
+                users[username] = password
+    except FileNotFoundError:
+        print("User file not found!")
+    return users
 
-username = input("Enter username: ")
-password = getpass.getpass("Enter password: ")
+def login(users):
+    attempts = 0
 
-if username in users and users[username] == password:
-    print("Login successful ✅")
-    print(f"Welcome, {username}!")
-else:
-    print("Invalid username or password ❌")
+    while attempts < MAX_ATTEMPTS:
+        username = input("Enter username: ")
+        password = getpass.getpass("Enter password: ")
+
+        if username in users and users[username] == password:
+            print("Login successful ✅")
+            print(f"Welcome, {username}!")
+            return True
+        else:
+            attempts += 1
+            print(f"Invalid credentials ❌ Attempts left: {MAX_ATTEMPTS - attempts}")
+
+    print("Too many failed attempts. Access blocked 🔒")
+    return False
+
+
+def main():
+    print("===== Library Management System =====")
+    users = load_users()
+    login(users)
+
+if __name__ == "__main__":
+    main()
