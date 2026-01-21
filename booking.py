431f3e91a99a2f0d26e98c30b6c65901b8c1d6cd
RISHI <rishianumuthu2006@gmail.com>
2026-01-21 23:23:03 +0530

    Ver.1

---
+++ booking.py
@@ -0,0 +1,8 @@
+def book_pg(name, location, rent):
+    print("PG Booking Confirmed")
+    print("Name:", name)
+    print("Location:", location)
+    print("Monthly Rent:", rent)
+
+# Sample booking
+book_pg("Ravi", "Chennai", 6500)
